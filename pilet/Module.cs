fcf7d2fa6f0c9891a93ae167485b443f35c6623c
Bill Hilke <whilke@gmail.com>
2023-01-30 20:39:03 -0600

    repo case - blazor HRM for css

---
+++ pilet/Module.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace pilet
+{
+	public class Module
+	{
+		public static void Main()
+		{
+			// this entrypoint should remain empty
+		}
+
+		public static void ConfigureServices(IServiceCollection services)
+		{
+			// configure dependency injection here
+		}
+	}
+}
